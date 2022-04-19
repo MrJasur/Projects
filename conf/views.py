b67540a6c26ceec4c1b05c0bcdd2ca3ba8fb6179
MrJasur <jasurbekodilovn@gmail.com>
2022-04-19 17:50:08 +0900

    home not finished

--- conf/views.py
+++ conf/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from books.models import Book, BookReview
 
 def landing_page(request):
     return render(request, 'landing.html')
+
+def home_page(request):
+    book_review = BookReview.objects.all().order_by('created_at')
+    context = {
+        'book_review':book_review
+    }
+    return render(request, 'home.html', context)
